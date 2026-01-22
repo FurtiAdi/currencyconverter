cdbf99412ab892d7c1ff2931971d1c658a528227
FurtiAdi <furtiadi@gmail.com>
2026-01-23 00:01:47 +0100

    Add User Repo

--- backend/src/main/java/com/example/currencyconverter/repository/UserRepository.java
+++ backend/src/main/java/com/example/currencyconverter/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.currencyconverter.repository;
 
-public class UserRepository {
+import com.example.currencyconverter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByApiKey(String apiKey);
+
 }
